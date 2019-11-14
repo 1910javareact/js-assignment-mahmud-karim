d4bdc90d2315130d5bdc99ffd689576cc27a67c1
mahmud <mahmudskarim@gmail.com>
2019-11-13 20:17:28 -0500

    palindrome done

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,17 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    //string to store reversed string
+    let output = ""
+    let pal = false
+    //reverses the string
+    for(let i = someStr.length; i > -1; i--){
+        output += someStr.charAt(i)
+    }
+    //if the reverse is the same as the string make pal true
+    if (output === someStr){
+        pal = true
+    }
+    //returns outputgit 
+    return pal
 }
