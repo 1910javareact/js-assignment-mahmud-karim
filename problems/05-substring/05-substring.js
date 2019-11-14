9eed51a47fd03e544357373d52e62e7bcdad376c
mahmud <mahmudskarim@gmail.com>
2019-11-13 19:45:36 -0500

    substring done

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,18 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
+    //declare varibles
+    let len = someStr.length
+    let output = ""
+    //Index was not a number
+    if (isNaN(startIndex) || isNaN(endIndex)){
+        throw "input was incorrect";
+    }
+    //gets the substring
+    for(let i = startIndex; i < endIndex; i++){
+        output += someStr.charAt(i)
+    }
+    //outputs new variable with wanted substring
+    return output
 
 }
