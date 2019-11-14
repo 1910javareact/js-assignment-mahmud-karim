e98af877947f625128de193f048c5df660508c08
mahmud <mahmudskarim@gmail.com>
2019-11-14 09:22:27 -0500

    splice fixed

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -5,21 +5,12 @@ Splice the element at the provided index in the array.
 function spliceElement(someArr, index) {
     //creates new array and copy over everything
     let newArr = [...someArr]
-    //temporary variable to store old value
-    let temp = newArr[index+1]
-    //adds the new element
-    newArr[index+1] = 'new element added'
-    //creates new index
-    let newIndex = index +2
-    //copies over the rest of array
-    for (let i = newIndex; i < someArr.length; i++) {
-        newArr[i] = temp
-        temp = newArr[i+1]
+    //copies next element to ith spot
+    for (let i = index; i < newArr.length; i++) {
+        newArr[i] = newArr[i+1]
     }
+    //removes last element
+    newArr.pop()
     //returns new array
     return newArr
 }
-
-let arr = [5,67,4,3,8]
-
-console.log(spliceElement(arr, 1));
