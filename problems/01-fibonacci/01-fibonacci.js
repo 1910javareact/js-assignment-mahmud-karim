3ba8ace1882201acadfebc8918396c40d854c947
mahmud <mahmudskarim@gmail.com>
2019-11-13 19:02:26 -0500

    added comments

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,7 +2,9 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+    //Varibles declared
     let a = 0, b = 1, temp;
+    //Goes through the numbers adding numbers up
     for (let i = n; i > 0; i--) {
         temp = a
         a = a + b
