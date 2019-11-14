7d9ec55ea2b91dd4af697122afd1758b4173a4a5
mahmud <mahmudskarim@gmail.com>
2019-11-13 22:15:04 -0500

    shapes done

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,74 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  //switches bases one the shape input
+  switch (shape) {
+    case "Square":
+      //prints the lines aka height
+      for(let i = 0; i < height; i++){
+        let temp = ""
+        //adds the same number of characters as the height to the temp string
+        for(let j = 0;j < height; j++){
+          temp += character
+        }
+        console.log(temp);
+      }
+      break;
+    case "Triangle":
+      //prints the lines aka height
+      for(let i = 0; i < height; i++){
+        let temp = ""
+        //adds one more each line
+        for(let j = i;j > -1; j--){
+          temp += character
+        }
+        console.log(temp);
+      }
+      break;
+    case "Diamond":
+      //star variable keeps track of the number of stars
+      let star = 0
+      //half variable for printing out the first half
+      let half = Math.floor(height/2) +1
+      //sechalf varialbe for printing out the second hald
+      let secHalf = Math.floor(height/2)
+      //spaces varible for keeping track of the spaces
+      let spaces = half - 1
+      //prints the first half of the diamond
+      for(let i = 0; i < half; i++){
+        let temp = ""
+        //adds space
+        for(let j = 0;j < spaces; j++){
+          temp += " "
+        }
+        //prints out the character accoring to the star variable
+        for(let j = 0;j <= star; j++){
+          temp += character
+        }
+        //decrments the spaces variable and increments the star variable
+        spaces--
+        star += 2
+        console.log(temp);
+      }
+      //resests the spaces and star
+      spaces = 0
+      star = half
+      //prints out the second half of the diamond
+      for(let i = secHalf; i > 0; i--){
+        let temp = " "
+        //prints the spaces
+        for(let j = 0;j < spaces; j++){
+          temp += " "
+        }
+        //prints the stars
+        for(let j = 0;j < star; j++){
+          temp += character
+        }
+        //increments the spaces and decrements the star
+        spaces++
+        star -= 2
+        console.log(temp);
+      }
+      break;
+  }
 }
