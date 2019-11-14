f11d31e2297bb8b1e0ee904ee851f525e22dc584
mahmud <mahmudskarim@gmail.com>
2019-11-13 20:12:41 -0500

    even number done

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,13 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    //divides it in half and gets the floor
+    let even = Math.floor(someNum/2)
+    //even numbers will have the same floor as regular division
+    if (even === someNum/2){
+        return true
+    }
+    return false
 }
+
+
